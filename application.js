50adaa2efa1ecc076da60307926591373de958bc
Ronan Limon Duparcmeur <ronan@2-45.pm>
2023-10-17 15:25:24 +0200

    Added the JS logic

--- application.js
+++ application.js
@@ -24,3 +24,44 @@ Stimulus.register("options-carousel", class extends Controller {
     this.fieldTarget.value = this.options[this.selectedIndex].value;
   }
 });
+
+
+Stimulus.register("options-table", class extends Controller {
+  static targets = [ "field", "option" ];
+  
+  pick(e) {
+    const n = this.optionTargets.indexOf(e.currentTarget);
+    
+    this.doHighlight(n);
+    this.doPick(n);
+  }
+  
+  pickAtRandom() {
+    const pickIndex = Math.floor(Math.random() * this.optionTargets.length);
+    
+    let iterations = Math.floor(Math.random() * 10);
+    let highlightIndex = 0;
+
+    const interval = setInterval(() => {
+      if (iterations > 0 || highlightIndex != pickIndex) {
+        iterations--;
+        
+        highlightIndex = (highlightIndex + 1) % this.optionTargets.length;
+        this.doHighlight(highlightIndex);
+      } else {
+        this.doPick(pickIndex);
+        clearInterval(interval);
+      }
+    }, 50);
+  }
+
+  doHighlight(n) {
+    this.optionTargets.forEach(t => t.classList.remove("selected"));
+    this.optionTargets[n].classList.add("selected");
+  }
+  
+  doPick(n) {
+    const pick = this.optionTargets[n];
+    this.fieldTarget.value = pick.querySelector("td").innerText;
+  }
+});
