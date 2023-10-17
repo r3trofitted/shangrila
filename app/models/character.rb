26ffac10533add8246e267d00adca2c681486814
Ronan Limon Duparcmeur <ronan@2-45.pm>
2023-10-17 19:18:58 +0200

    i18n
    Note how easily the UI adapts to longer translations, like in French. At
    least, when the UI is well-coded (i.e. in the "random" button, and not
    as in the legends).

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,2 +1,27 @@
 class Character < ApplicationRecord
+  ROLES = %i[
+    exec
+    fixer
+    lawman
+    media
+    medtech
+    netrunner
+    nomad
+    rockerboy
+    solo
+    tech
+  ]
+  
+  ORIGINS = %i[
+    north_american
+    south/central_american
+    western_europe
+    eastern_europe
+    middle_eastern/north_african
+    sub-saharan_african
+    south_asian
+    south-east_asian
+    east_asian
+    oceania/pacific_islander
+  ]
 end
