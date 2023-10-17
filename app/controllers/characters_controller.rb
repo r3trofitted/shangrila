293668fe556001f68870d7131326497b8c4ae321
Ronan Limon Duparcmeur <ronan@2-45.pm>
2023-10-17 22:20:58 +0200

    Setup submission
    Note an unexpected issue: URL anchors aren't handled by Turbo redirects
    (see https://github.com/hotwired/turbo/issues/211), so Turbo has to be
    skipped. which is too bad. TODO: find a solution to scroll to the anchor.
    
    (Plus find a way for the edit action to figure out _which anchor_ to
    use.)

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -4,11 +4,23 @@ class CharactersController < ApplicationController
   end
 
   def create
+    @character = Character.new(character_params)
+    
+    if @character.save
+      redirect_to edit_character_path(@character, anchor: "personals")
+    end
   end
 
   def edit
+    @character = Character.find(params[:id])
   end
 
   def update
+  end
+  
+  protected
+  
+  def character_params
+    params.require(:character).permit!
   end
 end
