a23a42349c25caddd8f44e3bdbb594bac033305c
Ronan Limon Duparcmeur <ronan@2-45.pm>
2023-10-18 22:57:45 +0200

    The next buttons work
    "Work" meaing they save the form (updating *all* the attributes) and
    redirected to the requested step.

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,4 +1,6 @@
 class CharactersController < ApplicationController
+  before_action :set_character, only: %i[edit update]
+  
   def new
     @character = Character.new
   end
@@ -7,20 +9,26 @@ class CharactersController < ApplicationController
     @character = Character.new(character_params)
     
     if @character.save
-      redirect_to edit_character_path(@character, anchor: "personals")
+      redirect_to edit_character_path(@character, anchor: params.dig(:character, :next))
     end
   end
 
   def edit
-    @character = Character.find(params[:id])
   end
 
   def update
+    if @character.update character_params
+      redirect_to edit_character_path(@character, anchor: params.dig(:character, :next))
+    end
   end
   
   protected
   
+  def set_character
+    @character = Character.find(params[:id])
+  end
+  
   def character_params
-    params.require(:character).permit!
+    params.require(:character).except(:next).permit!
   end
 end
